82386fd24693ec44dc2ffe2ebb259e639f6170a5
GodfredAsa <goldenheroe1@yahoo.com>
2023-01-17 22:18:49 +0000

    implementing delete, update user functionality with screens or pages  by admin user

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -26,6 +26,11 @@ import {
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
   USER_DELETE_FAIL,
+
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_RESET,
 } from "../constants/UserConstants";
 
 export const UserLoginReducer = (state = {}, action) => {
@@ -135,6 +140,26 @@ export const DeleteUserReducer = (state = {}, action) => {
   
       case USER_DELETE_FAIL:
         return { loading: false, error: action.payload };
+  
+      default:
+        return state;
+    }
+  };
+
+
+export const UpdateUserReducer = (state = {user: {}}, action) => {
+    switch (action.type) {
+      case USER_UPDATE_REQUEST:
+        return { loading: true };
+  
+      case USER_UPDATE_SUCCESS:
+        return { loading: false, success: true};
+
+      case USER_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+
+      case USER_UPDATE_RESET:
+        return {user: {} };
   
       default:
         return state;
